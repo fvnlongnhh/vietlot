4b0ada8ca28b8a0f567a95b7c1a4111ad5465243
Long <30433791+fvnlongnhh@users.noreply.github.com>
2019-11-04 20:05:19 +0700

    new data

--- js/vietlot.js
+++ js/vietlot.js
@@ -1,39 +1,66 @@
 $(document).ready(function () {
 	"use strict";
-    var pos_gnavi = $('#gnavi').offset().top;
-
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > pos_gnavi) {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('#btn_top').fadeIn(300);
+            $('#header').addClass('active');
             $('#gnavi').addClass('fix');
-        }
-        else{
+        } else {
+            $('#btn_top').fadeOut(300);
+            $('#header').removeClass('active');
             $('#gnavi').removeClass('fix');
         }
     });
 
-    $('.slide_list').slick({
+    $('#btn_top').click(function () {
+        $('body,html').animate({scrollTop: 0}, 800);
+    });
+
+    $('.slide_list ul, .box03_list ul').slick({
     	dots: false,
-    	arrows: false,
+    	arrows: true,
 		slidesToShow: 1,
         speed: 500,
         pauseOnHover: false,
         pauseOnFocuse : false,
         margin: 0,
         fade: true,
-  		cssEase: 'linear',
         autoplay: true,
         autoplaySpeed: 5000
-        
     });
-    
 
-    $(window).load(function(){
-        AOS.init({ 
-            easing: 'ease',
-            once: true,
-            duration: 1000,
-            disable: 'mobile',
-        }); 
+     $('.box04_img ul').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 4,
+        speed: 500,
+        pauseOnHover: false,
+        pauseOnFocuse : false,
+        margin: 0,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        centerMode: true,
+        centerPadding: 0
+
+    });
+});
+
+// anchor animation
+$(window).bind('load',function(){
+    "use strict";
+    $(function(){
+        $('a[href^="#"]').click(function(){
+            if ($($(this).attr('href') ).length ) {
+                var p = $( $(this).attr('href') ).offset();
+                if($(window).width() > 769){
+                    $('html,body').animate({ scrollTop: p.top - 80}, 400);
+                }
+                else {
+                    $('html,body').animate({ scrollTop: p.top - 80}, 400);
+                }
+            }
+            return false;
+        });
     });
 });
 
