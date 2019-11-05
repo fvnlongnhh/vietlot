3742870486699b6cbe69fb4797fbf387af74d7d9
Công Việc <fvn.longnhh@gmail.com>
2019-11-05 13:40:22 +0700

    done index

--- js/vietlot.js
+++ js/vietlot.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
         autoplaySpeed: 5000
     });
 
-     $('.box04_img ul').slick({
+    $('.box04_img ul').slick({
         dots: false,
         arrows: true,
         slidesToShow: 4,
@@ -41,8 +41,36 @@ $(document).ready(function () {
         autoplaySpeed: 5000,
         centerMode: true,
         centerPadding: 0
-
     });
+
+    $('.box05_list ul').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 3,
+        speed: 500,
+        pauseOnHover: false,
+        pauseOnFocuse : false,
+        margin: 0,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        centerMode: true,
+        centerPadding: 0
+    });
+
+    $('.box08_ct ul').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 7,
+        speed: 500,
+        pauseOnHover: false,
+        pauseOnFocuse : false,
+        margin: 0,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        centerMode: true,
+        centerPadding: 0
+    });
+
 });
 
 // anchor animation
