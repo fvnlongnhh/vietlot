1954527cf7f52ed012ab0ff9c4a9b9118c83b85c
Công Việc <fvn.longnhh@gmail.com>
2019-11-07 12:30:04 +0700

    done page Mua Ngay + Lich su mua ve

---
+++ js/main.js
@@ -0,0 +1,84 @@
+$(document).ready(function () {
+	"use strict";
+    $('.box03_list ul').slick({
+    	dots: false,
+    	arrows: true,
+		slidesToShow: 1,
+        speed: 500,
+        pauseOnHover: false,
+        pauseOnFocuse : false,
+        margin: 0,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 5000
+    });
+
+    $('.box04_img ul').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 4,
+        speed: 500,
+        pauseOnHover: false,
+        pauseOnFocuse : false,
+        margin: 0,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        centerMode: true,
+        centerPadding: 0
+    });
+
+    $('.box05_list ul').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 3,
+        speed: 500,
+        pauseOnHover: false,
+        pauseOnFocuse : false,
+        margin: 0,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        centerMode: true,
+        centerPadding: 0
+    });
+
+    $('.box08_ct ul').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 7,
+        speed: 500,
+        pauseOnHover: false,
+        pauseOnFocuse : false,
+        margin: 0,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        centerMode: true,
+        centerPadding: 0
+    });
+
+});
+
+// anchor animation
+$(window).bind('load',function(){
+    "use strict";
+    $(function(){
+        $('a[href^="#"]').click(function(){
+            if ($($(this).attr('href') ).length ) {
+                var p = $( $(this).attr('href') ).offset();
+                if($(window).width() > 769){
+                    $('html,body').animate({ scrollTop: p.top - 80}, 400);
+                }
+                else {
+                    $('html,body').animate({ scrollTop: p.top - 80}, 400);
+                }
+            }
+            return false;
+        });
+    });
+});
+
+
+
+
+
+
+
--- js/vietlot.js
+++ js/vietlot.js
@@ -1,5 +1,5 @@
-$(document).ready(function () {
-	"use strict";
+$(document).ready(function() {
+    "use strict";
     $(window).scroll(function() {
         if ($(this).scrollTop() > 100) {
             $('#btn_top').fadeIn(300);
@@ -12,89 +12,36 @@ $(document).ready(function () {
         }
     });
 
-    $('#btn_top').click(function () {
-        $('body,html').animate({scrollTop: 0}, 800);
+    $('#btn_top').click(function() {
+        $('body,html').animate({ scrollTop: 0 }, 800);
     });
 
-    $('.slide_list ul, .box03_list ul').slick({
-    	dots: false,
-    	arrows: true,
-		slidesToShow: 1,
-        speed: 500,
-        pauseOnHover: false,
-        pauseOnFocuse : false,
-        margin: 0,
-        fade: true,
-        autoplay: true,
-        autoplaySpeed: 5000
-    });
-
-    $('.box04_img ul').slick({
-        dots: false,
-        arrows: true,
-        slidesToShow: 4,
-        speed: 500,
-        pauseOnHover: false,
-        pauseOnFocuse : false,
-        margin: 0,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        centerMode: true,
-        centerPadding: 0
-    });
-
-    $('.box05_list ul').slick({
-        dots: true,
-        arrows: false,
-        slidesToShow: 3,
-        speed: 500,
-        pauseOnHover: false,
-        pauseOnFocuse : false,
-        margin: 0,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        centerMode: true,
-        centerPadding: 0
-    });
-
-    $('.box08_ct ul').slick({
-        dots: true,
-        arrows: false,
-        slidesToShow: 7,
-        speed: 500,
-        pauseOnHover: false,
-        pauseOnFocuse : false,
-        margin: 0,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        centerMode: true,
-        centerPadding: 0
-    });
-
-});
-
-// anchor animation
-$(window).bind('load',function(){
-    "use strict";
-    $(function(){
-        $('a[href^="#"]').click(function(){
-            if ($($(this).attr('href') ).length ) {
-                var p = $( $(this).attr('href') ).offset();
-                if($(window).width() > 769){
-                    $('html,body').animate({ scrollTop: p.top - 80}, 400);
-                }
-                else {
-                    $('html,body').animate({ scrollTop: p.top - 80}, 400);
-                }
-            }
-            return false;
+    if( $('#slider').length) {
+        $('.slide_list ul').slick({
+            dots: false,
+            arrows: true,
+            slidesToShow: 1,
+            speed: 500,
+            pauseOnHover: false,
+            pauseOnFocuse: false,
+            margin: 0,
+            fade: true,
+            autoplay: true,
+            autoplaySpeed: 5000
         });
+    }
+
+
+    $('.games_item').on('click', function() {
+        var href = $(this).find('a').attr('href');
+        location.href = href;
     });
+
+    $('.btn_modal').click(function (){
+        $('.box_modal, body').addClass('active');
+    })
+
+    $('.box_modal .close_modal').click(function (){
+        $('.box_modal, body').removeClass('active');
+    })
 });
-
-
-
-
-
-
-
